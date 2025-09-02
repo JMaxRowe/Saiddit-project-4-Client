33ccdea6b983043caa389c61605dfde9263759d9
JMaxRowe <jmaxrowe@gmail.com>
2025-09-02 23:05:33 +0100

    votes.js made

--- src/components/PostTile/PostTile.jsx
+++ src/components/PostTile/PostTile.jsx
@@ -1,11 +1,14 @@
 import './PostTile.css'
 import { Link } from "react-router";
+import { useEffect, useState } from 'react'
+import { setVote } from '../../utils/votes'
+import { getToken } from '../../utils/auth'
 
 export default function PostTile ({post}){
 
     return(
         <Link to={`/posts/${post.id}/`}>
-            <div className="postCard">
+            <div className="postCard" onClick={(e)=>e.stopPropagation()}>
                 <div className="communityInfo">
                     <p>{post.community.name}</p>
                     <p>{post.poster.username}</p>

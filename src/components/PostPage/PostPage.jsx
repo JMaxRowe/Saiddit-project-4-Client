6ac9f25436f6f0209b7b5f5d14388a2e0bd09b8a
JMaxRowe <jmaxrowe@gmail.com>
2025-09-02 13:01:19 +0100

    added comments to post page

--- src/components/PostPage/PostPage.jsx
+++ src/components/PostPage/PostPage.jsx
@@ -3,10 +3,12 @@ import { useEffect, useState, useContext } from "react";
 import { getPost, deletePost } from '../../utils/posts';
 import { Link, useParams, useNavigate } from "react-router";
 import { UserContext } from '../../contexts/UserContext';
+import { topLevelComments } from '../../utils/comments';
 
 export default function PostPage(){
     const { postId } = useParams()
     const [post, setPost] = useState(null)
+    const [comments, setComments] = useState([])
     const [isLoading, setIsLoading] = useState(false)
     const [error, setError] = useState(null)
     const { user } = useContext(UserContext)
@@ -27,8 +29,24 @@ export default function PostPage(){
             getPostData()
     }, [postId])
 
+    useEffect(() => {
+        const getCommentData = async () => {
+            setIsLoading(true)
+            try {
+                const { data } = await topLevelComments(postId)
+                setComments(data)
+            } catch (err) {
+                console.log(err)
+                setError(err)
+            } finally {
+                setIsLoading(false)
+            }
+            }
+            getCommentData()
+    }, [postId])
+
     if (isLoading) return <p>Loading…</p>
-    if (error) return <p>Post not found</p>
+    if (error) return <p>Post not found </p>
     if (!post) return <p>Where's the post?</p>
 
     return(
@@ -37,8 +55,24 @@ export default function PostPage(){
                 <h1>{post.title}</h1>
             </div>
             <div className="commentsSection">
-                <h2>With some comments</h2>
-            </div>
+                    {isLoading 
+                    ? 'Loading...'
+                    :
+                    comments.length > 0 ? (
+                        comments.map((comment) => {
+                            return (
+                            <div key={comment.id} className="commentTile">
+                                commenter: {comment.commenter.username} <br />
+                                {comment.body}
+                            </div>
+                            );
+                        })
+                        ) : (
+                        <p>There are no comments</p>
+                        )
+                    }
+                    
+                </div>
             
         </main>
     )
